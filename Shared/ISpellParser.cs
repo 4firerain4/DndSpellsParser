53b46aa6573c9b9483111db657e8509de955323d
4firerain4 <4firerain4@gmail.com>
2024-08-29 11:58:49 +0300

    Interface  configured

---
+++ Shared/ISpellParser.cs
@@ -0,0 +1,10 @@
+using ISpellParser;
+
+namespace Shared
+{
+    public interface ISpellParser
+    {
+        double Progress { get; }
+        Task<IEnumerable<Spell>> ParseSpellsAsynk();
+    }
+}
